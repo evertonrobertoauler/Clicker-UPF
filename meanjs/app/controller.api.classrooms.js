ce966716067cee4fc85d30b77a1e0deba402811c
Everton Roberto Auler <evertonrobertoauler@gmail.com>
2014-11-26 10:28:07 -0200

    refactoring my controllers to ES6

--- meanjs/app/controller.api.classrooms.js
+++ meanjs/app/controller.api.classrooms.js
@@ -29,73 +29,63 @@
     }));
   };
 
-  exports.list = function(req, res) {
-    Q.spawn(function*() {
-      try {
-        var filter = queries.filter(req.query);
-        filter.where = _.merge(req.query.where || {}, {'professor._id': req.user._id});
+  exports.list = Q.async(function*(req, res) {
+    try {
+      var filter = queries.filter(req.query);
+      filter.where = _.merge(req.query.where || {}, {'professor._id': req.user._id});
 
-        var data = yield queries.findList(Classroom, filter);
-        res.jsonp({length: data[0], list: data[1]});
-      } catch (e) {
-        console.trace(e);
-        res.status(400).jsonp(e);
-      }
-    });
-  };
+      var data = yield queries.findList(Classroom, filter);
+      res.jsonp({length: data[0], list: data[1]});
+    } catch (e) {
+      console.trace(e);
+      res.status(400).jsonp(e);
+    }
+  });
 
-  exports.insert = function(req, res) {
-    Q.spawn(function*() {
-      try {
-        var body = yield parseClassroom(req);
-        var classroom = new Classroom(body);
-        classroom.professor = req.user;
-        classroom = yield queries.exec(classroom, 'save');
-        res.status(201).jsonp(classroom);
-      } catch (e) {
-        console.trace(e);
-        res.status(400).jsonp(e);
-      }
-    });
-  };
+  exports.insert = Q.async(function*(req, res) {
+    try {
+      var body = yield parseClassroom(req);
+      var classroom = new Classroom(body);
+      classroom.professor = req.user;
+      classroom = yield queries.exec(classroom, 'save');
+      res.status(201).jsonp(classroom);
+    } catch (e) {
+      console.trace(e);
+      res.status(400).jsonp(e);
+    }
+  });
 
-  exports.get = function(req, res) {
-    Q.spawn(function*() {
-      try {
-        var classroom = yield getClassroom(req);
-        res.jsonp(classroom);
-      } catch (e) {
-        console.trace(e);
-        res.status(400).jsonp(e);
-      }
-    });
-  };
+  exports.get = Q.async(function*(req, res) {
+    try {
+      var classroom = yield getClassroom(req);
+      res.jsonp(classroom);
+    } catch (e) {
+      console.trace(e);
+      res.status(400).jsonp(e);
+    }
+  });
 
-  exports.update = function(req, res) {
-    Q.spawn(function*() {
-      try {
-        var data = yield Q.all([getClassroom(req), parseClassroom(req)]);
-        var classroom = _.extend(data[0], data[1]);
-        classroom = yield queries.exec(classroom, 'save');
-        task.schedule('updateOpenKnowledgeTest', [classroom._id]);
-        res.status(202).jsonp(classroom);
-      } catch (e) {
-        console.trace(e);
-        res.status(400).jsonp(e);
-      }
-    });
-  };
+  exports.update = Q.async(function*(req, res) {
+    try {
+      var data = yield Q.all([getClassroom(req), parseClassroom(req)]);
+      var classroom = _.extend(data[0], data[1]);
+      classroom = yield queries.exec(classroom, 'save');
+      task.schedule('updateOpenKnowledgeTest', [classroom._id]);
+      res.status(202).jsonp(classroom);
+    } catch (e) {
+      console.trace(e);
+      res.status(400).jsonp(e);
+    }
+  });
 
-  exports.delete = function(req, res) {
-    Q.spawn(function*() {
-      try {
-        var classroom = yield getClassroom(req);
-        yield queries.exec(classroom, 'remove');
-        res.status(204).send();
-      } catch (e) {
-        console.trace(e);
-        res.status(400).jsonp(e);
-      }
-    });
-  };
+  exports.delete = Q.async(function*(req, res) {
+    try {
+      var classroom = yield getClassroom(req);
+      yield queries.exec(classroom, 'remove');
+      res.status(204).send();
+    } catch (e) {
+      console.trace(e);
+      res.status(400).jsonp(e);
+    }
+  });
 })();
--- meanjs/app/controller.api.knowledgeTests.js
+++ meanjs/app/controller.api.knowledgeTests.js
@@ -41,82 +41,68 @@
     return queries.exec(Question.findOne({_id: knowledgeTest.question}));
   };
 
-  exports.list = function(req, res) {
+  exports.list = Q.async(function*(req, res) {
+    try {
+      var filter = queries.filter(req.query);
+      filter.where = _.merge(req.query.where || {}, {'professor._id': req.user._id});
 
-    var filter = queries.filter(req.query);
-    filter.where = _.merge(req.query.where || {}, {'professor._id': req.user._id});
+      var data = yield queries.findList(KnowledgeTest, filter);
+      res.jsonp({length: data[0], list: data[1]});
+    } catch (e) {
+      console.trace(e);
+      res.status(400).jsonp(e);
+    }
+  });
 
-    queries.findList(KnowledgeTest, filter)
-      .then(function(data) {
-        res.jsonp({length: data[0], list: data[1]});
-      })
-      .fail(function(err) {
-        console.trace(err);
-        res.status(400).jsonp(err);
-      });
-  };
+  exports.insert = Q.async(function*(req, res) {
+    try {
+      var body = yield parseInsert(req);
+      var data = yield Q.all([getClassroom(body), getQuestion(body)]);
 
-  exports.insert = function(req, res) {
+      var knowledgeTest = new KnowledgeTest(body);
+      knowledgeTest.professor = req.user;
+      knowledgeTest.classroom = data[0];
+      knowledgeTest.question = data[1];
+      knowledgeTest = yield queries.exec(knowledgeTest, 'save');
+      scheduleTasks(knowledgeTest);
+      res.status(201).jsonp(knowledgeTest);
+    } catch (e) {
+      console.trace(e);
+      res.status(400).jsonp(e);
+    }
+  });
 
-    parseInsert(req)
-      .then(function(data) {
-        return Q.all([data, getClassroom(data), getQuestion(data)]);
-      })
-      .then(function(data) {
-        var knowledgeTest = new KnowledgeTest(data[0]);
-        knowledgeTest.professor = req.user;
-        knowledgeTest.classroom = data[1];
-        knowledgeTest.question = data[2];
-        return queries.exec(knowledgeTest, 'save');
-      })
-      .then(function(knowledgeTest) {
-        scheduleTasks(knowledgeTest);
-        res.status(201).jsonp(knowledgeTest);
-      })
-      .fail(function(err) {
-        console.trace(err);
-        res.status(400).jsonp(err);
-      });
-  };
+  exports.get = Q.async(function*(req, res) {
+    try {
+      var knowledgeTest = yield getKnowledgeTest(req);
+      res.jsonp(knowledgeTest);
+    } catch (e) {
+      console.trace(e);
+      res.status(400).jsonp(e);
+    }
+  });
 
-  exports.get = function(req, res) {
-    getKnowledgeTest(req)
-      .then(function(data) {
-        res.jsonp(data);
-      })
-      .fail(function(err) {
-        console.trace(err);
-        res.status(400).jsonp(err);
-      });
-  };
+  exports.update = Q.async(function*(req, res) {
+    try {
+      var data = yield Q.all([getKnowledgeTest(req), parseUpdate(req)]);
+      var knowledgeTest = _.extend(data[0], data[1]);
+      knowledgeTest = yield queries.exec(knowledgeTest, 'save');
+      scheduleTasks(knowledgeTest);
+      res.status(202).jsonp(knowledgeTest);
+    } catch (e) {
+      console.trace(e);
+      res.status(400).jsonp(e);
+    }
+  });
 
-  exports.update = function(req, res) {
-    Q.all([getKnowledgeTest(req), parseUpdate(req)])
-      .then(function(data) {
-        var knowledgeTest = _.extend(data[0], data[1]);
-        return queries.exec(knowledgeTest, 'save');
-      })
-      .then(function(knowledgeTest) {
-        scheduleTasks(knowledgeTest);
-        res.status(202).jsonp(knowledgeTest);
-      })
-      .fail(function(err) {
-        console.trace(err);
-        res.status(400).jsonp(err);
-      });
-  };
-
-  exports.delete = function(req, res) {
-    getKnowledgeTest(req)
-      .then(function(knowledgeTest) {
-        return queries.exec(knowledgeTest, 'remove');
-      })
-      .then(function() {
-        return res.status(204).send();
-      })
-      .fail(function(err) {
-        console.trace(err);
-        res.status(400).jsonp(err);
-      });
-  };
+  exports.delete = Q.async(function*(req, res) {
+    try {
+      var knowledgeTest = yield getKnowledgeTest(req);
+      yield queries.exec(knowledgeTest, 'remove');
+      res.status(204).send();
+    } catch (e) {
+      console.trace(e);
+      res.status(400).jsonp(e);
+    }
+  });
 })();
