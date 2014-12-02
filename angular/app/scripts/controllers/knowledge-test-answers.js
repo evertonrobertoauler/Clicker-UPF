8011e8dbe39ad29e8d928fab367ceb66dad523fd
Everton Roberto Auler <evertonrobertoauler@gmail.com>
2014-12-02 01:04:27 -0200

    some frontend adjusts

--- angular/app/scripts/controllers/knowledge-test-answers.js
+++ angular/app/scripts/controllers/knowledge-test-answers.js
@@ -8,6 +8,17 @@ angular
     $scope.knowledgeTestAnswer.start = new Date($scope.knowledgeTestAnswer.start);
     $scope.knowledgeTestAnswer.end = new Date($scope.knowledgeTestAnswer.end);
 
+    var answer = $scope.knowledgeTestAnswer.answers[0];
+    $scope.knowledgeTestAnswer.answer = answer.answer;
+    $scope.knowledgeTestAnswer.triedAnswers = answer.triedAnswers;
+
+    $scope.$watch('knowledgeTestAnswer.answer', function(value){
+      if (answer.answer !== value) {
+        answer.answer = value;
+        $scope.save();
+      }
+    });
+
     $scope.save = function() {
 
       var error = function(e) {
